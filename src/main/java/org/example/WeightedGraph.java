b9b0ca576de7b23d652be1f678d17d5407191538
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-05-28 01:01:20 +0500

    WeightedGraph comments

--- src/main/java/org/example/WeightedGraph.java
+++ src/main/java/org/example/WeightedGraph.java
@@ -7,19 +7,24 @@ import java.util.Map;
 
 public class WeightedGraph<Vertex> {
     private boolean undirected;
-    private Map<Vertex,Map<Vertex,Double>> map = new HashMap<>();
+    private Map<Vertex, Map<Vertex, Double>> map = new HashMap<>();
+
     public WeightedGraph() {
         this(true);
     }
+
     public WeightedGraph(boolean undirected) {
         this.undirected = undirected;
     }
 
+    // adds a vertex to the graph
     public void addVertex(Vertex v) {
         map.put(v, new HashMap<>());
     }
 
+    // adds an edge between two vertices with a specified weight
     public void addEdge(Vertex source, Vertex dest, double weight) {
+        // Ensure both vertices exist in the graph
         if (!hasVertex(source))
             addVertex(source);
 
@@ -29,8 +34,10 @@ public class WeightedGraph<Vertex> {
         if (hasEdge(source, dest) || source.equals(dest))
             return;
 
-        addVertex(source);
-        map.get(source).put(dest,weight);
+        map.get(source).put(dest, weight);
+
+        if (undirected)
+            map.get(dest).put(source, weight);
     }
 
     private boolean hasEdge(Vertex source, Vertex dest) {
@@ -38,9 +45,9 @@ public class WeightedGraph<Vertex> {
         return map.get(source).containsKey(dest);
     }
 
+    // Checks if a vertex exists in the graph
     private boolean hasVertex(Vertex source) {
         return map.containsKey(source);
     }
 
-    // to be continued
 }
