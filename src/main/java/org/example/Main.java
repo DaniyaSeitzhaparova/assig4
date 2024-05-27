7fe8040ab675183389fea5c53d462817c43f5a8b
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-05-28 01:05:15 +0500

    Main comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,29 @@ public class Main {
     public static void main(String[] args) {
         WeightedGraph<String> graph = new WeightedGraph<>(true);
 
+        // adding edges with weights to the graph
         graph.addEdge("Almaty", "Astana", 3.9);
         graph.addEdge("Almaty", "Shymkent", 5.4);
         graph.addEdge("Shymkent", "Astana", 2.1);
         graph.addEdge("Astana", "Kostanay", 7.2);
         graph.addEdge("Shymkent", "Kyzylorda", 3.5);
 
-        System.out.println("Dijkstra:");
+        // performing Dijkstra's algorithm on the graph
         Search<String> djk = new DijkstraSearch<>(graph, "Almaty");
         outputPath(djk, "Kyzylorda");
 
-        System.out.println("DFS:");
+        // performing Depth-First Search on the graph
         Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
         outputPath(dfs, "Kyzylorda");
 
         System.out.println("\n--------------------------------");
 
-        System.out.println("BFS:");
+        // performing Breadth-First Search on the graph
         Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
         outputPath(bfs, "Kyzylorda");
     }
 
+    // printing the path
     public static void outputPath(Search<String> search, String key) {
         for (String v : search.pathTo(key)) {
             System.out.print(v + " -> ");
