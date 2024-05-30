97409d82957d9d56d866b43e99c50e88efe5bb87
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-05-30 06:59:34 +0500

    BreadthFirstSearch commments

--- src/main/java/org/example/BreadthFirstSearch.java
+++ src/main/java/org/example/BreadthFirstSearch.java
@@ -5,13 +5,18 @@ package classes;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
 public class BreadthFirstSearch<Vertex> extends Search<Vertex>{
+    //Constructs the BFS searcher for the given graph starting from the source vertex.
     public BreadthFirstSearch(WeightedGraph<Vertex> graph, Vertex source) {
         super(source);
 
         bfs(graph, source);
     }
 
+
+
+     // Performs BFS on the graph from the given starting vertex
     private void bfs(WeightedGraph<Vertex> graph, Vertex current) {
         marked.add(current);
 
