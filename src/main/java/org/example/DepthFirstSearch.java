8ef9ae21716720df29ba2b0260add346a67952ed
DaniyaSeitzhaparova <231502@astanait.edu.kz>
2024-05-30 07:01:38 +0500

    DepthFirstSearch comments

--- src/main/java/org/example/DepthFirstSearch.java
+++ src/main/java/org/example/DepthFirstSearch.java
@@ -3,12 +3,15 @@ package org.example;
 package classes;
 
 public class DepthFirstSearch<Vertex> extends Search<Vertex> {
+    //Constructs the DFS searcher for the given graph starting from the source vertex
+
     public DepthFirstSearch(WeightedGraph<Vertex> graph, Vertex source) {
         super(source);
 
         dfs(graph, source);
     }
 
+    //Performs DFS on the graph from the given starting vertex
     private void dfs(WeightedGraph<Vertex> graph, Vertex current) {
         marked.add(current);
 
